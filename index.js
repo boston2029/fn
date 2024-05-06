5802436ac257f063ec65ccebf5dab52417b5138f
Boston M <69646049+boston2029@users.noreply.github.com>
2024-05-05 22:37:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('X-Frame-Options', 'ALLOWALL');
+  next();
+});
 
 const nggUrl = 'https://mathsspot.com';
 
